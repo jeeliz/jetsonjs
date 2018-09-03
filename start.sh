9f2ad45e5ef18870e73f18f6535191e0040d21ee
xavierjs <xavier.bourry@polytechnique.org>
2018-09-03 22:34:00 +0000

    disable DPMS and cursor

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 export DISPLAY=:0.0
-xinit -e matchbox-window-manager-2-simple -nocursor -use_titlebar no &
+xinit -e matchbox-window-manager-2-simple -use_titlebar no -- -nocursor &
+xset dpms force off &
+xset s off &
+xset s noblank &
 cd ./server;./startServer.sh $1 :0.0
